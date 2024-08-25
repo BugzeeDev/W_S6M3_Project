c40b3ef1d9df59b22e678ab30ae556b6690a03cf
BugzeeDev <irvingcsylva@gmail.com>
2024-08-25 03:59:43 -0700

    fixed hardcoded API call

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -22,16 +22,16 @@ function App() {
         console.log(err)
       })
     }
-    // getPhoto()
-    setApod({
-        "date": "2024-08-25",
-        "explanation": "Do underground oceans vent through canyons on Saturn's moon Enceladus?  Long features dubbed tiger stripes are known to be spewing ice from the moon's icy interior into space, creating a cloud of fine ice particles over the moon's South Pole and creating Saturn's mysterious E-ring.  Evidence for this has come from the robot Cassini spacecraft that orbited Saturn from 2004 to 2017.  Pictured here, a high resolution image of Enceladus is shown from a close flyby.  The unusual surface features dubb...",
-        "hdurl": "https://apod.nasa.gov/apod/image/2408/EnceladusStripes_Cassini_3237.jpg",
-        "media_type": "image",
-        "service_version": "v1",
-        "title": "Fresh Tiger Stripes on Saturn's Enceladus",
-        "url": "https://apod.nasa.gov/apod/image/2408/EnceladusStripes_Cassini_960.jpg"
-      })
+    getPhoto()
+    // setApod({
+    //     "date": "2024-08-25",
+    //     "explanation": "Do underground oceans vent through canyons on Saturn's moon Enceladus?  Long features dubbed tiger stripes are known to be spewing ice from the moon's icy interior into space, creating a cloud of fine ice particles over the moon's South Pole and creating Saturn's mysterious E-ring.  Evidence for this has come from the robot Cassini spacecraft that orbited Saturn from 2004 to 2017.  Pictured here, a high resolution image of Enceladus is shown from a close flyby.  The unusual surface features dubb...",
+    //     "hdurl": "https://apod.nasa.gov/apod/image/2408/EnceladusStripes_Cassini_3237.jpg",
+    //     "media_type": "image",
+    //     "service_version": "v1",
+    //     "title": "Fresh Tiger Stripes on Saturn's Enceladus",
+    //     "url": "https://apod.nasa.gov/apod/image/2408/EnceladusStripes_Cassini_960.jpg"
+    //   })
   }, [])
 
   if (!apod) return 'Fetching Photo of the Day...'
